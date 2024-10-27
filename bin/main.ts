4723b95ae1c8a8d499b2259bff2a342eff47cd12
LauricaFaraFrica <laurentiuginghina@outlook.com>
2024-10-27 06:51:19 +0200

    feat: wip

--- bin/main.ts
+++ bin/main.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { BaselineInfrastructure } from '../lib/base-infrastructure-stack';
+import { BaseVPCInfrastructure } from '../lib/base-Infrastructure-stack';
 
 const app = new cdk.App();
 
 // Create baseInfrastructure stack
-new BaselineInfrastructure(app, "BaselineInfrastructure", {
+new BaseVPCInfrastructure(app, "BaselineInfrastructure", {
     env: {
         region: "us-west-2"
     }
